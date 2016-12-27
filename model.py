599ed272e06a0b1eddda95c073464eaa328f593f
Artem Larionov <larionov.artem@gmail.com>
2016-12-27 12:24:14 -0600

    improved model with batch normalization

--- model.py
+++ model.py
@@ -1,46 +1,55 @@
 import json
 import math
+import random
 import numpy as np
 import pandas as pd
 import cv2
 import click
 
-from keras.models import Sequential
-from keras.layers import Activation, Dense, Dropout, Flatten, SpatialDropout2D
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
 from keras.callbacks import ModelCheckpoint
+from keras.layers import Dense, Dropout, Flatten, SpatialDropout2D
+from keras.layers.advanced_activations import PReLU
+from keras.layers.convolutional import Convolution2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D
+from keras.models import Sequential
 from keras.optimizers import Adam
-from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 def preprocessing(image):
+    """ Returns cropped, resized and normalized image """
     image = image[65:135,:]
     image = cv2.resize(image, None, fx = 0.125, fy = 0.5, interpolation = cv2.INTER_CUBIC)
     image = (image.astype(np.float32) - 128.0) / 128.0
     return image
 
-def get_data(df):
-    images = []
-    angles = []
+def data_generator(df, batch_size):
+    """ The generator goes through the dataframe
+    and yields lists of images and angles of set batch size.
+    The generator also flip the image along x axis with 50% chance.
+    """
+    while 1:
+        images = []
+        angles = []
+        for index, row in df.sample(batch_size).iterrows():
+            image = cv2.imread(row['image'].strip(), -1)
+            image = preprocessing(image)
+            angle = row['angle']
 
-    for index, row in df.iterrows():
-        image = cv2.imread(row['image'].strip(), -1)
-        image = preprocessing(image)
-        angle = row['angle']
+            if random.randrange(2) > 0:
+                image = cv2.flip(image, 1)
+                angle = -angle
 
-        images.append(image)
-        angles.append(angle)
+            images.append(image)
+            angles.append(angle)
 
-        image = cv2.flip(image, 1)
-        angle = -angle
-
-        images.append(image)
-        angles.append(angle)
-
-    return (np.array(images), np.array(angles))
+        images = np.array(images)
+        angles = np.array(angles)
+        yield (images, angles)
 
 def read_csv(path, short_image_path = False):
+    """ Returns dataframe of driving logs from csv file """
     df = pd.read_csv(path + 'driving_log.csv').ix[:,0:4]
     df.columns = ['center', 'left', 'right', 'angle']
 
@@ -52,12 +61,16 @@ def read_csv(path, short_image_path = False):
     return df
 
 def left_angle(angle, angle_factor):
+    """ Returns the angle for the left image based on the angle for the central image"""
     return angle + abs(angle * angle_factor) + 7 * math.pi / 180.0
 
 def right_angle(angle, angle_factor):
+    """ Returns the angle for the right image based on the angle for the central image"""
     return angle - abs(angle * angle_factor) - 7 * math.pi / 180.0
 
 def extract_data(df):
+    """ Returns dataframe of images and steering angles extracted from driving logs dataframe
+    """
     angle_factor = 0.25
     rows = []
 
@@ -72,14 +85,27 @@ def extract_data(df):
     return pd.DataFrame(rows)
 
 def build_model(input_shape, dropout = 0.5):
+    """ Returns the model """
     model = Sequential()
     model.add(Convolution2D( 3, 1, 1, border_mode = 'same',  subsample = (1, 1), init = 'uniform', input_shape = input_shape))
-    model.add(Convolution2D(24, 5, 5, border_mode = 'valid', subsample = (2, 2), init = 'uniform'))
-    model.add(Activation('relu'))
+    model.add(Convolution2D(36, 6, 6, border_mode = 'same', subsample = (1, 1), init = 'uniform'))
+    model.add(PReLU())
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D())
     model.add(SpatialDropout2D(dropout))
+    model.add(Convolution2D(42, 6, 6, border_mode = 'valid', subsample = (2, 2), init = 'uniform'))
+    model.add(PReLU())
+    model.add(BatchNormalization())
+    model.add(Convolution2D(48, 3, 3, border_mode = 'valid', subsample = (2, 2), init = 'uniform'))
+    model.add(PReLU())
+    model.add(BatchNormalization())
     model.add(Flatten())
-    model.add(Dense(50, init = 'normal'))
-    model.add(Activation('relu'))
+    model.add(Dense(300, init = 'normal'))
+    model.add(PReLU())
+    model.add(Dense(150, init = 'normal'))
+    model.add(PReLU())
+    model.add(Dense( 50, init = 'normal'))
+    model.add(PReLU())
     model.add(Dropout(dropout))
     model.add(Dense(1, activation='tanh'))
     return model
@@ -89,37 +115,41 @@ def build_model(input_shape, dropout = 0.5):
 @click.option('--weights', default = None, help = 'Path to a file with default weights.')
 @click.option('--batch',   default = 512,  help = 'Batch size.')
 @click.option('--epochs',  default = 10,   help = 'Number of epochs.')
-def model(path, weights, batch, epochs):
-    udacity_data = read_csv(path, True)
-    driving_data = extract_data(udacity_data)
+@click.option('--lr',      default = 1e-4, help = 'Learning rate.')
+def model(path, weights, batch, epochs, lr):
+    """ Main function to train the model """
 
-    features, labels = get_data(driving_data)
-    features, labels = shuffle(features, labels, random_state = 1)
+    # read logs and extracting images and steering angles
+    logs = read_csv(path, True)
+    data = extract_data(logs)
 
-    train_features, valid_features, train_labels, valid_labels = \
-        train_test_split(features, labels, test_size=0.33, random_state = 1)
+    # split data into training set(66%) and validation set(33%)
+    train, valid = train_test_split(data, test_size=0.33, random_state = 1)
 
+    # build and compile the model
     model = build_model((35, 40, 3))
-    model.compile(loss = 'mse', optimizer = Adam(1e-5))
+    model.compile(loss = 'mse', optimizer = Adam(lr))
 
+    # set predefined weights if required
     if weights:
         model.load_weights(weights)
 
+    # save weights after each epoch if accuracy increased on validation set
     checkpoint = ModelCheckpoint('model.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
     callbacks  = [checkpoint]
 
-    model.fit(
-        train_features,
-        train_labels,
-        batch_size = batch,
-        nb_epoch = epochs,
+    # train the model
+    model.fit_generator(
+        generator         = data_generator(train, batch),
+        samples_per_epoch = train.shape[0],
+        max_q_size        = batch,
+        nb_epoch          = epochs,
         callbacks         = callbacks,
-        validation_data = (
-            valid_features,
-            valid_labels
-        )
+        validation_data   = data_generator(valid, batch),
+        nb_val_samples    = valid.shape[0]
     )
 
+    # save the model into JSON file
     model_json = model.to_json()
     with open('model.json', 'w') as outfile:
         json.dump(model_json, outfile)
